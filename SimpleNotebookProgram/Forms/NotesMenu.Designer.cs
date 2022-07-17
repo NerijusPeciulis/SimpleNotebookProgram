1ce06d7eaa64065d5b83de16018e3d84e296ee5e
Nerijus <Npeciulis@yahoo.com>
2022-07-17 17:45:38 +0300

    add filter function to notes

--- SimpleNotebookProgram/Forms/NotesMenu.Designer.cs
+++ SimpleNotebookProgram/Forms/NotesMenu.Designer.cs
@@ -29,61 +29,71 @@
         private void InitializeComponent()
         {
             this.label4 = new System.Windows.Forms.Label();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.deleteNoteButton = new System.Windows.Forms.Button();
+            this.editNoteButton = new System.Windows.Forms.Button();
+            this.createNoteButton = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.notesTextbox = new System.Windows.Forms.TextBox();
+            this.noteSearchTextBox = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.noteNameTextbox = new System.Windows.Forms.TextBox();
+            this.noteNameLabel = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.noteDescription = new System.Windows.Forms.TextBox();
+            this.dataNotesGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataNotesGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label4.Location = new System.Drawing.Point(221, 9);
+            this.label4.Location = new System.Drawing.Point(396, 9);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(134, 30);
             this.label4.TabIndex = 13;
             this.label4.Text = "Notes Menu";
             // 
-            // button3
+            // deleteNoteButton
             // 
-            this.button3.Location = new System.Drawing.Point(152, 174);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(162, 23);
-            this.button3.TabIndex = 12;
-            this.button3.Text = "Delete";
-            this.button3.UseVisualStyleBackColor = true;
+            this.deleteNoteButton.Location = new System.Drawing.Point(193, 478);
+            this.deleteNoteButton.Name = "deleteNoteButton";
+            this.deleteNoteButton.Size = new System.Drawing.Size(162, 23);
+            this.deleteNoteButton.TabIndex = 12;
+            this.deleteNoteButton.Text = "Delete";
+            this.deleteNoteButton.UseVisualStyleBackColor = true;
+            this.deleteNoteButton.Click += new System.EventHandler(this.deleteNoteButton_Click);
             // 
-            // button2
+            // editNoteButton
             // 
-            this.button2.Location = new System.Drawing.Point(152, 130);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(162, 23);
-            this.button2.TabIndex = 11;
-            this.button2.Text = "Edit";
-            this.button2.UseVisualStyleBackColor = true;
+            this.editNoteButton.Location = new System.Drawing.Point(193, 426);
+            this.editNoteButton.Name = "editNoteButton";
+            this.editNoteButton.Size = new System.Drawing.Size(162, 23);
+            this.editNoteButton.TabIndex = 11;
+            this.editNoteButton.Text = "Edit";
+            this.editNoteButton.UseVisualStyleBackColor = true;
+            this.editNoteButton.Click += new System.EventHandler(this.editNoteButton_Click);
             // 
-            // button1
+            // createNoteButton
             // 
-            this.button1.Location = new System.Drawing.Point(152, 84);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(162, 23);
-            this.button1.TabIndex = 10;
-            this.button1.Text = "Create";
-            this.button1.UseVisualStyleBackColor = true;
+            this.createNoteButton.Location = new System.Drawing.Point(193, 84);
+            this.createNoteButton.Name = "createNoteButton";
+            this.createNoteButton.Size = new System.Drawing.Size(162, 23);
+            this.createNoteButton.TabIndex = 10;
+            this.createNoteButton.Text = "Create";
+            this.createNoteButton.UseVisualStyleBackColor = true;
+            this.createNoteButton.Click += new System.EventHandler(this.button1_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(12, 123);
+            this.label3.Location = new System.Drawing.Point(12, 419);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(100, 30);
             this.label3.TabIndex = 9;
@@ -93,7 +103,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(12, 167);
+            this.label2.Location = new System.Drawing.Point(12, 471);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(125, 30);
             this.label2.TabIndex = 8;
@@ -113,7 +123,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label6.Location = new System.Drawing.Point(12, 258);
+            this.label6.Location = new System.Drawing.Point(12, 577);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(110, 30);
             this.label6.TabIndex = 18;
@@ -123,53 +133,118 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label7.Location = new System.Drawing.Point(12, 212);
+            this.label7.Location = new System.Drawing.Point(12, 532);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(127, 30);
             this.label7.TabIndex = 19;
             this.label7.Text = "Search Note";
             // 
-            // textBox1
+            // notesTextbox
             // 
-            this.textBox1.Location = new System.Drawing.Point(395, 77);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(542, 424);
-            this.textBox1.TabIndex = 20;
+            this.notesTextbox.Location = new System.Drawing.Point(613, 77);
+            this.notesTextbox.Multiline = true;
+            this.notesTextbox.Name = "notesTextbox";
+            this.notesTextbox.Size = new System.Drawing.Size(385, 424);
+            this.notesTextbox.TabIndex = 20;
             // 
-            // textBox2
+            // noteSearchTextBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(152, 219);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(162, 23);
-            this.textBox2.TabIndex = 21;
+            this.noteSearchTextBox.Location = new System.Drawing.Point(193, 539);
+            this.noteSearchTextBox.Name = "noteSearchTextBox";
+            this.noteSearchTextBox.Size = new System.Drawing.Size(162, 23);
+            this.noteSearchTextBox.TabIndex = 21;
+            this.noteSearchTextBox.TextChanged += new System.EventHandler(this.noteSearchTextBox_TextChanged);
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(152, 265);
+            this.textBox3.Location = new System.Drawing.Point(193, 586);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(162, 23);
             this.textBox3.TabIndex = 22;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(12, 121);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(121, 30);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Note Name";
+            // 
+            // noteNameTextbox
+            // 
+            this.noteNameTextbox.Location = new System.Drawing.Point(193, 130);
+            this.noteNameTextbox.Name = "noteNameTextbox";
+            this.noteNameTextbox.Size = new System.Drawing.Size(162, 23);
+            this.noteNameTextbox.TabIndex = 24;
+            // 
+            // noteNameLabel
+            // 
+            this.noteNameLabel.AutoSize = true;
+            this.noteNameLabel.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.noteNameLabel.Location = new System.Drawing.Point(748, 44);
+            this.noteNameLabel.Name = "noteNameLabel";
+            this.noteNameLabel.Size = new System.Drawing.Size(125, 30);
+            this.noteNameLabel.TabIndex = 25;
+            this.noteNameLabel.Text = "Create Note";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(12, 171);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(170, 30);
+            this.label8.TabIndex = 26;
+            this.label8.Text = "Note Description";
+            // 
+            // noteDescription
+            // 
+            this.noteDescription.Location = new System.Drawing.Point(193, 175);
+            this.noteDescription.Name = "noteDescription";
+            this.noteDescription.Size = new System.Drawing.Size(162, 23);
+            this.noteDescription.TabIndex = 27;
+            // 
+            // dataNotesGridView
+            // 
+            this.dataNotesGridView.AllowUserToDeleteRows = false;
+            this.dataNotesGridView.AllowUserToOrderColumns = true;
+            this.dataNotesGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataNotesGridView.Location = new System.Drawing.Point(12, 204);
+            this.dataNotesGridView.Name = "dataNotesGridView";
+            this.dataNotesGridView.RowTemplate.Height = 25;
+            this.dataNotesGridView.Size = new System.Drawing.Size(534, 212);
+            this.dataNotesGridView.TabIndex = 28;
+            this.dataNotesGridView.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataNotesGridView_RowHeaderMouseClick);
             // 
             // NotesMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1010, 636);
+            this.Controls.Add(this.dataNotesGridView);
+            this.Controls.Add(this.noteDescription);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.noteNameLabel);
+            this.Controls.Add(this.noteNameTextbox);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.noteSearchTextBox);
+            this.Controls.Add(this.notesTextbox);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.deleteNoteButton);
+            this.Controls.Add(this.editNoteButton);
+            this.Controls.Add(this.createNoteButton);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "NotesMenu";
             this.Text = "NotesMenu";
+            this.Load += new System.EventHandler(this.NotesMenu_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataNotesGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -178,16 +253,22 @@
         #endregion
 
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button deleteNoteButton;
+        private System.Windows.Forms.Button editNoteButton;
+        private System.Windows.Forms.Button createNoteButton;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox notesTextbox;
+        private System.Windows.Forms.TextBox noteSearchTextBox;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox noteNameTextbox;
+        private System.Windows.Forms.Label noteNameLabel;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox noteDescription;
+        private System.Windows.Forms.DataGridView dataNotesGridView;
     }
 }
